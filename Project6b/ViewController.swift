7b0b64f7f71756b18708f723174b394caf6ca35b
Azat <kaumov.a.r@gmail.com>
2021-05-23 21:14:55 +0300

    Add metrics

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -48,6 +48,8 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
+        
+        let metrics = ["labelHeight": 88]
         let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
         
         for label in viewsDictionary.keys {
@@ -62,9 +64,9 @@ class ViewController: UIViewController {
         
         view.addConstraints(
             NSLayoutConstraint.constraints(
-                withVisualFormat: "V:|[label1(==88)]-[label2(==88)]-[label3(==88)]-[label4(==88)]-[label5(==88)]-(>=10)-|",
+                withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|",
                 options: [],
-                metrics: nil,
+                metrics: metrics,
                 views: viewsDictionary
             )
         )
